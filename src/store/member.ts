dc486467be5638232fa3c8c079e118eccf38c229
Rourou <mimikapple@gmail.com>
2019-05-03 18:52:46 +0800

    update

--- src/store/member.ts
+++ src/store/member.ts
@@ -1,10 +1,13 @@
-import { ITestMemberResponse } from '@/models/interfaces/member';
+// import { ITestMemberResponse } from '@/models/interfaces/member';
 import { IActionContextBasic } from '@/store/index';
 import MemberApi from '@/api/member';
 import FileApi from '@/api/file';
 import AuthApi from '@/api/auth';
+import * as Status from '@/models/status/member';
+import * as Model from '@/models/interfaces/member';
+
 export interface IState {
-    memberInfos: ITestMemberResponse[];
+    memberInfos: Model.ITestMemberResponse[];
 }
 const SEARCH_MEMBER = 'SEARCH_MEMBER';
 
@@ -19,7 +22,24 @@ const getters = {
 
 const actions = {
     getMember(context: IActionContextBasic, uuid: string) {
+        AuthApi.getMenu().then((data) => {
+            context.commit(SEARCH_MEMBER, data);
+        });
         FileApi.uploadFile('uuid','tertre').then((data) => {
+            context.commit(SEARCH_MEMBER, data);
+        });
+        FileApi.getFile('uuid').then((data) => {
+            context.commit(SEARCH_MEMBER, data);
+        });
+        MemberApi.updateMemberStatus(<Model.IMemberStatusRequest>{
+            accountAction: Status.AccountStatus.Normal,
+            withdrawalAction: Status.WithdrawalStatus.Normal,
+            reason: '',
+            files: []
+        },'tertre').then((data: boolean) => {
+            context.commit(SEARCH_MEMBER, data);
+        });
+        MemberApi.getMemberStatusHistoryList('uuid').then((data) => {
             context.commit(SEARCH_MEMBER, data);
         });
     },
