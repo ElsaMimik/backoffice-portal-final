162a85b37541e020901596ccc0616b31353a9c1d
rourou <rourou@starlordtech.com>
2019-05-08 02:22:58 +0800

    update

--- src/utilities/datetime-format.ts
+++ src/utilities/datetime-format.ts
@@ -6,19 +6,19 @@ export const datetimeMixin = {
             return iso;
         },
         timestampToDatetime(timestamp: number) {
-            var time = new Date(timestamp * 1000);
-            var hours = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
-            var minutes = `0${time.getMinutes()}`;
-            var seconds = `0${time.getSeconds()}`;
+            const time = new Date(timestamp * 1000);
+            const hours = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+            const minutes = `0${time.getMinutes()}`;
+            const seconds = `0${time.getSeconds()}`;
             return `${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
         },
         timestampToDateAndTime(timestamp: number) {
             const date = new Date(timestamp);
             const iso = date.toISOString().slice(0, 10).replace(/-/g, '-');
-            var time = new Date(timestamp * 1000);
-            var hours = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
-            var minutes = `0${time.getMinutes()}`;
-            var seconds = `0${time.getSeconds()}`;
+            const time = new Date(timestamp * 1000);
+            const hours = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+            const minutes = `0${time.getMinutes()}`;
+            const seconds = `0${time.getSeconds()}`;
             return `${iso}  ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
         },
     }
