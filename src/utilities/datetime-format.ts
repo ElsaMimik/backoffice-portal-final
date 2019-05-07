ae15ec0af5212d7c709f119d11e90f91ee960445
rourou <rourou@starlordtech.com>
2019-05-08 02:22:11 +0800

    update

--- src/utilities/datetime-format.ts
+++ src/utilities/datetime-format.ts
@@ -5,5 +5,21 @@ export const datetimeMixin = {
             const iso = date.toISOString().slice(0, 10).replace(/-/g, '-');
             return iso;
         },
+        timestampToDatetime(timestamp: number) {
+            var time = new Date(timestamp * 1000);
+            var hours = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+            var minutes = `0${time.getMinutes()}`;
+            var seconds = `0${time.getSeconds()}`;
+            return `${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+        },
+        timestampToDateAndTime(timestamp: number) {
+            const date = new Date(timestamp);
+            const iso = date.toISOString().slice(0, 10).replace(/-/g, '-');
+            var time = new Date(timestamp * 1000);
+            var hours = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+            var minutes = `0${time.getMinutes()}`;
+            var seconds = `0${time.getSeconds()}`;
+            return `${iso}  ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+        },
     }
 };
