d9cd7cdb7509bb64a235c9562693f3307fea27cb
Rourou <mimikapple@gmail.com>
2019-05-08 10:19:14 +0800

    update

---
+++ src/components/message/Confirm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="popup">
+    <div class="popup__view">
+      <div class="popup__close">
+        <div class="btns">
+          <button class="btns__close"></button>
+        </div>
+      </div>
+      <div class="popup__title">Warning</div>
+      <div class="popup__info dottedline-top">
+        <div class="popup__info-title">
+          <div class="popup__info-items">
+            <p>你確定要嗎</p>
+          </div>
+        </div>
+      </div>
+      <div class="popup__btn">
+        <div class="btns">
+          <button class="btns__submit">取消</button>
+          <button class="btns__submit">确定</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class ConfirmPopup extends Vue {}
+</script>
---
+++ src/components/message/Information.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="popup">
+    <div class="popup__view">
+      <div class="popup__close">
+        <div class="btns">
+          <button class="btns__close"></button>
+        </div>
+      </div>
+      <div class="popup__title">Information</div>
+      <div class="popup__info dottedline-top">
+        <div class="popup__info-title">
+          <div class="popup__info-items">
+            <p>詳述錯誤</p>
+          </div>
+        </div>
+      </div>
+      <div class="popup__btn">
+        <div class="btns">
+          <button class="btns__submit">确定</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class InformationPopup extends Vue {}
+</script>
