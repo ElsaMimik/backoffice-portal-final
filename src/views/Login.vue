4c8a9755150d815903a051a26157b7ae6ac9b364
Ellen <ellen@starlordtech.com>
2019-05-09 20:23:35 +0800

    no message

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,11 +13,7 @@ import { Component } from "vue-property-decorator";
 export default class Login extends Vue {
 
 	mounted() {
-<<<<<<< HEAD
 		Cookies.set('token','eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiIyMjIyMjIiLCJ1c2VyX2lkIjoiOGQzMjUwZmItOGU3ZS00MDNlLWFjNzYtNTczNWNlYjA5MjFhIiwiZXhwaXJlZF9hdCI6MTU1NzQwNzI1NH0.pTM1QsLAomjTiCiLBwneWWKJQVDQMUCLyl6M49Nq4hky0iMfXp4eKRcXkr-8NHfahYc_Ot2nXB4C0mfFve3-DA');
-=======
-		Cookies.set('token','eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiIyMjIyMjIiLCJ1c2VyX2lkIjoiOWZlMTc2ZmQtOTQ2ZC00YzVhLTk3MDQtYzgxNDVlMzM3NDY0IiwiZXhwaXJlZF9hdCI6MTU1NzQwNzY3M30.IYGubk6VkWd85_2kk4C9goR02SlEac8F2F1dFrXZCYHOgdRxqhLWCOzd9mhlp1KR2p9iXTpdmFhjinjUdkMIUg');
->>>>>>> origin/rourou/feat/BO-11
 	}
 }
 </script>
