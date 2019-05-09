6001a4dc312964643373e08676d1e8fd9b350c0f
Rourou <mimikapple@gmail.com>
2019-05-09 15:54:51 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router/index';
 import store from '@/store/index';
-require('@/assets/css/base/_reset.scss');
-require('@/assets/css/layout/_layout.scss');
-require('@/assets/css/components/_components.scss');
+import '@/assets/css/base/_reset.scss';
+import '@/assets/css/layout/_layout.scss';
+import '@/assets/css/components/_components.scss';
+
 Vue.config.productionTip = false;
 
 new Vue({
