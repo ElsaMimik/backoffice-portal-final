00aa142791e8cba9b35e32e3ce129b4678e08919
Ellen <ellen@starlordtech.com>
2019-05-09 17:23:06 +0800

    Detail

--- src/components/members/Detail.vue
+++ src/components/members/Detail.vue
@@ -105,33 +105,39 @@
 						{{ memberData.isBidWin | isBidWinDisplay }}
 					</div>
 				</div>
+				<div class="btns">
+					<button class="btns__green">更新</button>
+					<button class="btns__green">查询交易纪录</button>
+				</div>
 			</div>
-			<div class="btns">
-			<button :class="{ 'btns__green active': tab === 'Basic',
-						'btns__green': tab !== 'Basic'}" 
-						@click="changeTab('Basic')">
-				基本资料
-			</button>
-			<button :class="{ 'btns__green active': tab === 'Transaction',
-									'btns__green': tab !== 'Transaction'}" 
-									@click="changeTab('Transaction')">
-				交易资料
-			</button>
-			<button :class="{ 'btns__green active': tab === 'Login',
-						'btns__green': tab !== 'Login'}" 
-						@click="changeTab('Login')">
-				登入资料
-			</button>
-			<button :class="{ 'btns__green active': tab === 'Relations',
-						'btns__green': tab !== 'Relations'}" 
-						@click="changeTab('Relations')">
-				关联资料
-			</button>
-			<button :class="{ 'btns__green active': tab === 'RiskControl',
-						'btns__green': tab !== 'RiskControl'}" 
-						@click="changeTab('RiskControl')">
-				风控条件
-			</button>
+		</div>
+		<div class="member__detail-btns">
+			
+				<button :class="{ 'btns__green active': tab === 'Basic',
+							'btns__green': tab !== 'Basic'}" 
+							@click="changeTab('Basic')">
+					基本资料
+				</button>
+				<button :class="{ 'btns__green active': tab === 'Transaction',
+										'btns__green': tab !== 'Transaction'}" 
+										@click="changeTab('Transaction')">
+					交易资料
+				</button>
+				<button :class="{ 'btns__green active': tab === 'Login',
+							'btns__green': tab !== 'Login'}" 
+							@click="changeTab('Login')">
+					登入资料
+				</button>
+				<button :class="{ 'btns__green active': tab === 'Relations',
+							'btns__green': tab !== 'Relations'}" 
+							@click="changeTab('Relations')">
+					关联资料
+				</button>
+				<button :class="{ 'btns__green active': tab === 'RiskControl',
+							'btns__green': tab !== 'RiskControl'}" 
+							@click="changeTab('RiskControl')">
+					风控条件
+				</button>
 			</div>
 		</div>
 	</div>
